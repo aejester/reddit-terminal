2409b3c3ee8368a191341a645e57058bdc73bbd0
aejester <afterffectsjester@gmail.com>
2021-02-05 22:24:28 -0500

    Created more pages, configured routing. Installed react-router-dom, and prop-types.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { Component } from 'react';
 import './App.css';
+import NavigationBar from './components/navigation/NavigationBar';
+import LogInDelegator from './components/renderingDelegates/homePage/LogInDelegator';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
 	constructor(props) {
+
 		super(props);
+
 		this.state = {
 
 		}
+
 	}
 
 	render() {
@@ -14,7 +21,32 @@ class App extends Component {
 		return (
 
 			<div className="app">
+				<Router>
+					<NavigationBar/>
 				
+
+					<Switch>
+						<Route path="/">
+							<LogInDelegator showLogIn={true} />
+						</Route>
+
+						<Route path="/account">
+							{/*TODO: ACCOUNT*/}
+						</Route>
+
+						<Route path="/dashboard">
+							<Dashboard />
+						</Route>
+
+						<Route path="/signup">
+							<LogInDelegator showLogIn={false} />
+						</Route>
+
+						<Route path="/login">
+							<LogInDelegator showLogIn={true} />
+						</Route>
+					</Switch>
+				</Router>
 			</div>
 
 		);
